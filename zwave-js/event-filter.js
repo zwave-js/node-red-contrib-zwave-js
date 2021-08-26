37fa2ad168bab866faff8dbe87d4c7d076b1813a
Marcus Davies <55892693+marcus-j-davies@users.noreply.github.com>
2021-08-26 21:53:41 +0100

    Update event-filter.js

--- zwave-js/event-filter.js
+++ zwave-js/event-filter.js
@@ -8,10 +8,41 @@ module.exports = function (RED) {
 		node.on('input', Input);
 
 		async function Input(msg, send, done) {
-			send(msg);
-			if (done) {
-				done();
+
+
+			if(msg.payload !== undefined && msg.payload.event !== undefined){
+				send(msg);
 			}
+			else{
+				if (done) {
+					done();
+				}
+			}
+
+			
+			
+		}
+
+		function IsValueIDMatch(ValueID, MSG){
+
+			let Root = MSG.payload;
+			if(Root.hasOwnProperty("valueId")){
+				Root = MSG.payload.valueId;
+			}
+
+			const ValueIDKeys = Object.keys(ValueID)
+			const MSGKeys = Object.keys(Root)
+
+			ValueIDKeys.forEach((VIDK) =>{
+				if(MSGKeys.includes(VIDK)){
+					if(Root[VIDK] !== ValueID[VIDK]){
+						return false
+					}
+				}else{
+					return false;
+				}
+			})
+			return true;
 		}
 
 		node.on('close', (done) => {
