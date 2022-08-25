f4db7b05d7968764ac84d985c5c8319bea656f22
Marcus Davies <55892693+marcus-j-davies@users.noreply.github.com>
2022-08-25 20:27:11 +0100

    Update client.js

--- resources/UITab/client.js
+++ resources/UITab/client.js
@@ -2664,7 +2664,7 @@ const ZwaveJsUI = (function () {
 								const B = $(`<label for="Target_${F.target}">Target ${F.target}</label> <input type="radio" name="fw_target" value="${F.target}" id="Target_${F.target}"> `);
 								B.data('FWTarget',{file:F,node:Node});
 								Content.append(B)
-								B.checkboxradio();
+								//B.checkboxradio();
 							})
 							FWList.append(Content);
 						})
