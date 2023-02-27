01c8a3f959ba0c87a8366116cf867e5816d16150
Marcus Davies <55892693+marcus-j-davies@users.noreply.github.com>
2023-02-27 20:19:19 +0000

    Move get Ports to bootstrap instance

--- src/nodes/UIBootstrap.ts
+++ src/nodes/UIBootstrap.ts
@@ -1,4 +1,13 @@
 import { NodeAPI } from 'node-red';
-module.exports = (_: NodeAPI) => {
-    //
+import { Driver } from 'zwave-js';
+module.exports = (RED: NodeAPI) => {
+	RED.httpAdmin.get('/zwave-js/ui/global/getports', RED.auth.needsPermission('flows.write'), (_, response) => {
+		Driver.enumerateSerialPorts()
+			.then((data) => {
+				response.json({ callSuccess: true, response: data });
+			})
+			.catch((Error) => {
+				response.json({ callSuccess: false, response: Error.message });
+			});
+	});
 };
--- src/nodes/ZWaveJSRuntime.ts
+++ src/nodes/ZWaveJSRuntime.ts
@@ -962,15 +962,5 @@ module.exports = (RED: NodeAPI) => {
 			});
 	};
 
-	RED.httpAdmin.get('/zwave-js/ui/global/getports', RED.auth.needsPermission('flows.write'), (_, response) => {
-		Driver.enumerateSerialPorts()
-			.then((data) => {
-				response.json({ callSuccess: true, response: data });
-			})
-			.catch((Error) => {
-				response.json({ callSuccess: false, response: Error.message });
-			});
-	});
-
 	RED.nodes.registerType('zwavejs-runtime', init);
 };
