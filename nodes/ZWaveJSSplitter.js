4e2e4d21a474d7dff79dd025d2559e7de5f6824c
Marcus Davies <55892693+marcus-j-davies@users.noreply.github.com>
2025-11-23 09:07:20 +0000

    Update ZWaveJSSplitter.js

--- nodes/ZWaveJSSplitter.js
+++ nodes/ZWaveJSSplitter.js
@@ -7,24 +7,36 @@ module.exports = (RED) => {
 		self.on('input', (msg, send, done) => {
 			if (msg.payload?.event) {
 				let VID;
-
+				let Match;
+				let PinIndex;
 				switch (msg.payload?.event) {
 					case 'VALUE_UPDATED':
 					case 'VALUE_ADDED':
+					case 'VALUE_NOTIFICATION':
 						VID = msg.payload.eventBody.valueId;
+						Match = self.config.splits.find(
+							(V) =>
+								V.commandClass === VID.commandClass &&
+								V.property === VID.property &&
+								(VID.propertyKey === undefined || V.propertyKey === VID.propertyKey)
+						);
 						break;
 
 					case 'NOTIFICATION':
 						VID = msg.payload.eventBody.ccId;
 						break;
-
-					case 'VALUE_NOTIFICATION':
-						VID = msg.payload.eventBody.valueId;
-						break;
 				}
-			} else {
-				send(msg);
-				done();
+				if (Match) {
+					PinIndex = Match.index;
+					msg.split = {
+						name: Match.name
+					};
+
+					const out = new Array(self.config.splits.length);
+					out[PinIndex] = msg;
+					send(out);
+					done();
+				}
 			}
 		});
 
